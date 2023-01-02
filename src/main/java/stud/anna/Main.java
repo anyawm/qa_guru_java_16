bd0107f3892a41a71c73fbfabc4e617d59903c15
Anna Petrova <mail.anna.p@yandex.ru>
2023-01-02 20:53:40 +0500

    add for

--- src/main/java/stud/anna/Main.java
+++ src/main/java/stud/anna/Main.java
@@ -1,7 +1,43 @@
 package stud.anna;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        // массивы
+        int[] intArr0 = new int[]{1, 42, -100};
+        int[] intArr = new int[3];
+        intArr0[0] = 1;
+        intArr0[1] = 42;
+        intArr0[2] = -100;
+
+        System.out.println("Длина массива: " + intArr0.length);
+        System.out.println("Массив: " + Arrays.toString(intArr0));
+
+        for (int i = 0; i < intArr0.length; i++) {
+            System.out.println(i + "-й элемент массива: " + intArr0[i]);
+        }
+// в обратную сторону
+        for (int i = intArr0.length - 1; i >= 0; i--) {
+            System.out.println(i + "-й элемент массива: " + intArr0[i]);
+        }
+// прерывание цикла
+        for (int i = 0; i < intArr0.length; i++) {
+            if (intArr0[i] == -100) {
+                System.out.println("Нашли: " + intArr0[i]);
+                break;
+            }
+            System.out.println("Neat iteration");
+        }
+
+        for (int i = 0; i < intArr0.length; i++) {
+            if (intArr0[i] != -100) {
+                continue;
+            }
+            System.out.println("Нашли: " + intArr0[i]);
+            break;
+        }
+
         // целые числа
         byte varByte = 0; // 2 бита -128 ... 127
         short varShort = 0; // 8 бита -32768 ... 32767
@@ -44,21 +80,18 @@ public class Main {
         // 3. Сравнения >, <, >=, <=, !=, ==
         if (1 == 1) // сравнение идентичности, как и !=
 
-        // 4. Логические операторы работают с типом boolean && (и), || (или), ! (не)
-        if ((age >= 30) && (sex.equals("female")))
-            if (!(age >= 30) && (sex.equals("female")))) // ! меняет true на false
+            // 4. Логические операторы работают с типом boolean && (и), || (или), ! (не)
+            if ((age >= 30) && (sex.equals("female")))
+                if (!(age >= 30) && (sex.equals("female")))) // ! меняет true на false
         if ((age >= 30) || (sex.equals("female")))
 
-            if (varStr != null && varStr.length() ==10) {
+            if (varStr != null && varStr.length() == 10) {
                 System.out.println(varStr);
-            } else  if (varStr != null && varStr.length() ==9){
+            } else if (varStr != null && varStr.length() == 9) {
                 System.out.println("Извините, строка из 9 символов");
             } else {
                 System.out.println("Извините, строка не из 9 или 10 символов");
             }
-
-
-
 
 
         System.out.println("Hello world!");
